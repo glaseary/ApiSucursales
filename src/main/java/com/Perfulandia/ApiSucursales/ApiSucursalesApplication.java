8e744bd6db8ba0a0f785d54ad51d315110f9d7ee
glaseary <sofia.troncoso.m@gmail.com>
2025-06-14 23:45:22 -0400

    primer commit

---
+++ src/main/java/com/Perfulandia/ApiSucursales/ApiSucursalesApplication.java
@@ -0,0 +1,13 @@
+package com.Perfulandia.ApiSucursales;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiSucursalesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiSucursalesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Perfulandia/ApiSucursales/ApiSucursalesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Perfulandia.ApiSucursales;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiSucursalesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
